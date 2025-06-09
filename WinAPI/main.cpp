8aa7bb2fa6e6dfdeb0d07ea6364ecc178e3abf0c
sevru <sevru@DESKTOP-NFMFIIS>
2025-06-09 22:59:16 +1000

    Add project files.

---
+++ WinAPI/main.cpp
@@ -0,0 +1,47 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//#define MESSAGE_BOX
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst,LPSTR lpCmdLine, INT nCmdShow)
+{
+#ifdef MESSAGE_BOX 
+	//КОДИРОВКА АСКИ
+//1 параметр родительское окно, 2й сообщение,3 ЗАГОЛОВОК ОКНА,4 FLAGS BITS(отдельные биты которые мы можем комбинировать)
+	MessageBox
+	(
+		NULL,
+		"Испугался? не бойся!\n я друг))\nсамое простое окно \t'сообщение'",
+		"BooOoO"/*NULL*/,
+		MB_YESNOCANCEL | MB_ICONINFORMATION | MB_HELP | MB_DEFBUTTON3 | MB_SYSTEMMODAL
+	);
+#endif // MESSAGE_BOX 
+	 
+	DialogBoxParam(hInstance, MAKEINTRESOURCEA(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+	case WM_INITDIALOG://выполняется 1 раз при запуске окна
+		break;
+
+	case WM_COMMAND://обрабатывает нажатие кнопок, перемещение мыши и т.д
+
+		switch(LOWORD(wParam))
+		{
+		   case IDOK: MessageBox(hwnd, "Была нажата кнопка 'ok'", "Info", MB_OK | MB_ICONINFORMATION); break;
+		}
+		break;
+
+	case WM_CLOSE:/*отрабатывает при нажатии на кнопку закрыть*/
+
+		EndDialog(hwnd ,0);
+		break;
+	}
+	return FALSE;
+}
