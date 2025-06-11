a5bb6abb92b1d692634cebd29b4d84f9a4b603f6
sevru <sevru@DESKTOP-NFMFIIS>
2025-06-11 19:18:26 +1000

    moya golova chut ne vzorvalas, DZ DONE;

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,7 +1,9 @@
 #include<Windows.h>
 #include"resource.h"
 
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+bool isСlueShown = true;// глобал флаг на подсказку внутри поля
 
 //#define MESSAGE_BOX
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -25,13 +27,51 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+   
 	switch (uMsg)
 	{
 	case WM_INITDIALOG://выполняется 1 раз при запуске окна
+	{
+#define  EM_GETCUEBANNER "Введите Логин"
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SetWindowTextA(hEditLogin, EM_GETCUEBANNER);//if A(char*) else W(WCHAR_T*)
+
 		break;
+	}
+	case WM_COMMAND://обрабатывает нажатие кнопок, перемещение мыши и т.д//тут надо обрабатывать курсор
+	{
+		if (LOWORD(wParam) == IDC_EDIT_LOGIN)
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			CONST INT SIZE = 265;
+			CHAR sz_buffer[SIZE] = {};
 
-	case WM_COMMAND://обрабатывает нажатие кнопок, перемещение мыши и т.д
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:
+			{
+				// Удаляем подсказку, если она сейчас отображается
+				GetWindowTextA(hEditLogin, sz_buffer, sizeof(sz_buffer));
+				if (strcmp(sz_buffer, EM_GETCUEBANNER) == 0)
+				{
+					SetWindowTextA(hEditLogin, "");
+				}
+				break;
+			}
+			case EN_KILLFOCUS:
+			{
+				// Если пользователь не ввёл ничего — возвращаем подсказку
+				GetWindowTextA(hEditLogin, sz_buffer, sizeof(sz_buffer));
+				if (strlen(sz_buffer) == 0)
+				{
+					SetWindowTextA(hEditLogin, EM_GETCUEBANNER);
+				}
+				break;
+			}
 
+			}
+		}
+	}
 		switch (LOWORD(wParam))
 		{
 		case IDC_BUTTON_COPY:
@@ -42,18 +82,21 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CHAR sz_buffer[SIZE] = {};
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-		}	
-		break;
-		case IDOK: MessageBox(hwnd, "Была нажата кнопка 'ok'", "Info", MB_OK | MB_ICONINFORMATION); break;
-		case IDCANCEL:EndDialog(hwnd, 0);
-			break;
 		}
 		break;
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка 'ok'", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
 
-	case WM_CLOSE:/*отрабатывает при нажатии на кнопку закрыть*/
+			break;
 
-		EndDialog(hwnd, 0);
-		break;
+		case WM_CLOSE:/*отрабатывает при нажатии на кнопку закрыть*/
+
+			EndDialog(hwnd, 0);
+			break;
+		}
 	}
+	
 	return FALSE;
-}
+	}
